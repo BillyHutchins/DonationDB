2876de88cfead4a3dcbac75c2031203971263318
Billy Hutchins <bhutchins@spscommerce.com>
2020-01-13 15:32:37 -0500

    Added Donations database and table

---
+++ DonationsDB/DonationsDB/dbo/Tables/Donations.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Donations] (
+    [Id]     INT           IDENTITY (1, 1) NOT NULL,
+    [Name]   NVARCHAR (50) NULL,
+    [Email]  NVARCHAR (50) NULL,
+    [Amount] NVARCHAR (10) NULL
+);
+
